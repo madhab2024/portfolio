1d39078baf94271777a847bad150dfd431799ef5
Madhab2024 <mondalmadhab4231@gmail.com>
2026-03-15 03:00:26 +0530

    CI/CD test 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -120,7 +120,7 @@ const Hero = () => {
                     >
                         {/* Hidden H1 for SEO */}
                         <h1 style={{ position: 'absolute', width: '1px', height: '1px', padding: 0, margin: '-1px', overflow: 'hidden', clip: 'rect(0, 0, 0, 0)', whiteSpace: 'nowrap', border: 0 }}>
-                            Madhab Mondal - Full Stack Developer
+                            Madhab Mondal - Full Stack Developer (AWS Certified)
                         </h1>
                         <div className="image-wrapper">
                             <img src={profileImg} alt="Madhab Mondal - Full Stack Developer Profile" className="profile-img" />
